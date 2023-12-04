b16033cbda2713d32a27e634177c7b969904ca0f
Carlos Barrales Ruiz <cbruiz@gmail.com>
2023-12-04 23:02:55 +0100

    Replace pwm todo! by dummies

--- hwi-boards/printhor-hwi_native/src/board/mocked_peripherals/mocked_pwm.rs
+++ hwi-boards/printhor-hwi_native/src/board/mocked_peripherals/mocked_pwm.rs
@@ -17,13 +17,13 @@ impl MockedPwm {
 }
 
 impl Pwm for MockedPwm {
-    fn set_period<P>(&mut self, _: P) where P: Into<u16> { todo!() }
-    fn set_duty(&mut self, _: <Self as Pwm>::Channel, _: <Self as Pwm>::Duty) { todo!() }
-    fn get_max_duty(&self) -> <Self as Pwm>::Duty { todo!() }
-    fn get_duty(&self, _: <Self as Pwm>::Channel) -> <Self as Pwm>::Duty { todo!() }
-    fn get_period(&self) -> <Self as Pwm>::Time { todo!() }
-    fn enable(&mut self, _: <Self as Pwm>::Channel) { todo!() }
-    fn disable(&mut self, _: <Self as Pwm>::Channel) { todo!() }
+    fn set_period<P>(&mut self, _: P) where P: Into<u16> {  }
+    fn set_duty(&mut self, _: <Self as Pwm>::Channel, _: <Self as Pwm>::Duty) {  }
+    fn get_max_duty(&self) -> <Self as Pwm>::Duty { 0u16  }
+    fn get_duty(&self, _: <Self as Pwm>::Channel) -> <Self as Pwm>::Duty { 0u16  }
+    fn get_period(&self) -> <Self as Pwm>::Time { 0u16 }
+    fn enable(&mut self, _: <Self as Pwm>::Channel) { }
+    fn disable(&mut self, _: <Self as Pwm>::Channel) {  }
     type Duty = u16;
     type Time = u16;
     type Channel = u8;
